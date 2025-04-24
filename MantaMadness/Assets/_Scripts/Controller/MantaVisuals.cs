c2b442c678ec88b7489860cd573109126ba8e90d
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2025-04-24 10:55:01 +0200

    Update model and water shader

---
+++ MantaMadness/Assets/_Scripts/Controller/MantaVisuals.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class MantaVisuals : MonoBehaviour
+{
+    SimpleController mantaController;
+
+    public Transform modelTransform;
+
+    private void Awake()
+    {
+        mantaController = GetComponent<SimpleController>();
+    }
+
+    private void Update()
+    {
+        UpdateModelRoll();
+    }
+
+    private void UpdateModelRoll()
+    {
+        float angular = mantaController.AngularVelocity.y;
+
+        modelTransform.localRotation = Quaternion.Euler(0, 0, -angular * 10);
+
+    }
+}
