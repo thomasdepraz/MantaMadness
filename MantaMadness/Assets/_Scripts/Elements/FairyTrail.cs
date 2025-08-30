d264263eff52da51864ca883b9796c61d46adac2
Job Corentin <job.corentin.pro@gmail.com>
2025-08-30 23:24:45 +0200

    Controller data improv + new fairy trail ld element

---
+++ MantaMadness/Assets/_Scripts/Elements/FairyTrail.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class FairyTrail : MonoBehaviour
+{
+    [SerializeField] private SplineAnimate splinePlayer;
+    [SerializeField] private SplineContainer path;
+    [SerializeField] private ParticleSystem sparkles;
+    [SerializeField] private Transform body;
+    [SerializeField] private GameObject[] toActivate;
+
+    private bool hasActivated =  false;
+
+    private void Start()
+    {
+        if (toActivate.Length > 0) 
+        {
+            foreach (var item in toActivate)
+            {
+                item.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out SimpleController controller))
+        {
+            if(hasActivated == false)
+            {
+                hasActivated = true;
+                StartCoroutine(spawnCoroutine());
+                StartCoroutine(Timer());
+            }
+        }
+    }
+
+    private IEnumerator spawnCoroutine()
+    {
+        splinePlayer.Play();
+
+        for (int i = 0; i < toActivate.Length; i++)
+        {
+            if(i == 0)
+            {
+                yield return new WaitForSeconds(splinePlayer.Duration / (toActivate.Length + 1));
+                toActivate[i].gameObject.SetActive(true);
+                toActivate[i].gameObject.transform.position = body.transform.position;
+                yield return new WaitForSeconds(splinePlayer.Duration / (toActivate.Length + 1));
+            }        
+            //else if (i == toActivate.Length - 2)
+            //{
+            //    toActivate[i].gameObject.SetActive(true);
+            //    toActivate[i].gameObject.transform.position = body.transform.position;
+            //    yield return new WaitForSeconds(splinePlayer.Duration / (toActivate.Length + 1));
+            //}
+            else
+            {
+                toActivate[i].gameObject.SetActive(true);
+                toActivate[i].gameObject.transform.position = body.transform.position;
+                yield return new WaitForSeconds(splinePlayer.Duration / (toActivate.Length + 1));
+            }
+        }
+        yield return null;
+    }
+
+    private IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(splinePlayer.Duration);
+        splinePlayer.Pause();
+        sparkles.Stop();
+    }
+
+}
